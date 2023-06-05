2a37bc3c1356264d71d895969ddf4e83c920b36c
Vladimir Semikov <voesem@ya.ru>
2023-06-05 21:42:16 +0300

    дописаны классы Stack и Node (файл main.py падает с ошибкой), написан тест к методу push класса Stack

--- src/stack.py
+++ src/stack.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,7 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.stack = []
 
     def push(self, data):
         """
@@ -23,7 +24,7 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        self.stack.append(data)
 
     def pop(self):
         """
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,18 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+
+from src.stack import Stack
+
+
+class TestStack(unittest.TestCase):
+
+    def test_push(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        self.assertEqual(stack.stack, ['data1', 'data2'])
+        stack.push('data3')
+        self.assertEqual(stack.stack, ['data1', 'data2', 'data3'])
+
+
+if __name__ == '__main__':
+    unittest.main()
